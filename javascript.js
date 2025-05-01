c68f8a663d9e7bd9e3d4cc1a10085ff144493f60
Raymond Zilli <raymondzilli@Raymonds-MacBook-Pro.local>
2025-04-30 21:23:22 -0400

    add search feature for new gif

--- javascript.js
+++ javascript.js
@@ -16,3 +16,45 @@ let btn = document.querySelector("#searchBtn");
 btn.addEventListener('click',()=>{  
         cats(); 
 } );
+
+//search for new pictures. 
+function searchGif() {
+    const searchTerm = document.querySelector("#searchValue").value;
+    const apiKey = 'nBfq2TYOaFaJdwoQhqqAAxRE8qUcEyhu'; 
+  
+    if (!searchTerm.trim()) {
+      console.log("Please enter a search term.");
+      return;
+    }
+  
+    const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(searchTerm)}&limit=10`;
+  
+    fetch(apiUrl)
+      .then(response => { 
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log("Giphy Search Results JSON:", data);
+        if (data.data && data.data.length > 0) {
+          const randomPlace = Math.floor(Math.random() * data.data.length );
+          img.src = data.data[randomPlace].images.original.url; // get random GIF from search 
+        } else {
+          console.log("No GIFs found in the search results.");
+         
+          img.src = "#"; // Clear the image source
+        }
+      })
+      .catch(error => {
+        console.error("Error fetching GIFs:", error);
+      });
+  }
+
+// trigger an event when search is clicked 
+let gifSearch = document.querySelector("#gifSearch");
+gifSearch.addEventListener('click', (e)=>{
+    e.preventDefault(); 
+     searchGif(); 
+})
